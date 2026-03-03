0f447def99902646363a4bf1f1e162d9eab76563
Opnek90 <155686271+Opnek90@users.noreply.github.com>
2026-03-03 23:18:41 +0100

    chore: add PWA and compression plugins to vite.config.ts

--- services/webui-service/vite.config.ts
+++ services/webui-service/vite.config.ts
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+import viteCompression from 'vite-plugin-compression';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Minabox WebUI',
+        short_name: 'Minabox',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        background_color: '#ffffff',
+      }
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+    }),
+    viteCompression({
+      algorithm: 'gzip',
+      ext: '.gz',
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
